c7088e6ddcbf4070d917564ed2274e94b5796705
sajnabhandari <123648542+sajnabhandari@users.noreply.github.com>
2025-04-28 08:39:46 -0500

    Update main.py
    ver1 of main

--- main.py
+++ main.py
@@ -1,27 +1,8 @@
-def menu():
-    while True:
-        print("Welcome user!")
-        print("1. Option 1")
-        print("2. Option 2")
-        print("3. Option 3")
-        print("4. Option 4")
-        print("0. Exit")
+from bank_account import BankAccount //
+def main():
+    user_id = input("Welcome! Please enter your account user ID: ")
+    account = BankAccount(user_id)
+    account.run_menu()
 
-        option = input("Please enter the coressponding number-")
-
-        if option == "0":
-            print("Exiting...")
-            print("Goodbye!")
-        elif option == "1":
-            option1()
-        elif option == "2":
-            option2()
-        elif option == "3":
-            option3()
-        elif option == "4":
-            option4()
-    
-# def option1():
-# def option2():
-# def option3():
-# def option4():
+if __name__ == "__main__":
+    main()
