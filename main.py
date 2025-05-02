123a88c21301abcceac1100207b204f7a1f152a4
Sajna Bhandari <bhandarisajna@yahoo.com>
2025-05-01 19:45:00 -0500

    new changes + testing 1

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-from bank_account import BankAccount 
+# main function to start the banking app
+from bank_account import BankAccount # import the BankAccount class
+#main functions are stored in bank_account.py, to make things organized(?)
 def main():
+    # ask user for their id
     user_id = input("Welcome! Please enter your account user ID: ")
+    # create BankAccount object and start CLI menu
     account = BankAccount(user_id)
     account.run_menu()
 
